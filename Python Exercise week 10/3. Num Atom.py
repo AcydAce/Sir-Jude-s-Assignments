ba5c49bee2d81245512ee45e6fc65c1c8dba0a5c
AcydAce <bayuhartho@gmail.com>
2021-10-27 21:26:19 +0700

    no. 3 completed

---
+++ Python Exercise week 10/3. Num Atom.py
@@ -0,0 +1,12 @@
+def num_atom(amount,weight=196.97):
+
+    avogadro = 6.022 * 10 ** 23
+    form = amount * avogadro / weight
+
+    return form
+
+
+print("the atomic weight of:\n")
+print("gold:",num_atom(10),"\n")
+print("carbon:",num_atom(10,12.001),"\n")
+print("hydrogen:",num_atom(10,1.008),"\n")
--- Python Exercise week 10/Exercise week 10.py
+++ Python Exercise week 10/Exercise week 10.py
@@ -19,10 +19,12 @@ get_day = seconds_to_day + minute_to_day + hour_to_day
 print("the number of days is %.4f::" % float(get_day))
 """
 
-
+"""
 def calc_weight_on_planet(weight, grav=23.1):
+
     base = 9.8
     form = weight / base * grav
+    
     return form
 
 
@@ -31,3 +33,17 @@ print(calc_weight_on_planet(120,9.8) , end= "lb\n")
 print(calc_weight_on_planet(120,) , end= "lb\n")
 
 print(calc_weight_on_planet(120,23.1) , end= "lb")
+"""
+
+def num_atom(amount,weight=196.97):
+
+    avogadro = 6.022 * 10 ** 23
+    form = amount * avogadro / weight
+
+    return form
+
+
+print("the atomic weight of:\n")
+print("gold:",num_atom(10),"\n")
+print("carbon:",num_atom(10,12.001),"\n")
+print("hydrogen:",num_atom(10,1.008),"\n")
