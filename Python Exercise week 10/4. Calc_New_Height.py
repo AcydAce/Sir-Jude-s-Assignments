a30be1ac9e660281c1b353ab7ff0542e3d38a8ea
AcydAce <bayuhartho@gmail.com>
2021-10-27 21:42:02 +0700

    no. 4 polished

---
+++ Python Exercise week 10/4. Calc_New_Height.py
@@ -0,0 +1,10 @@
+def calc_new_height():
+
+    width = int(input("Enter the current width: "))
+    height = int(input("Enter the current height: "))
+    new_width = int(input("Enter the new width: "))
+    new_res = height / width * new_width
+
+    return new_res
+
+print("The new height is: ", calc_new_height())
